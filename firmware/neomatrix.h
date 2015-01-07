1d02055ed1e2087ae5db72faaf5870ef9a1a0290
Serge Sozonoff <serge@sozonoff.com>
2015-01-07 23:56:36 +0100

    update

--- firmware/neomatrix.h
+++ firmware/neomatrix.h
@@ -22,7 +22,7 @@
 //Needed for Spark Core...the previous includes are for the web IDE.
 #include "application.h"
 #include "../Adafruit_GFX/Adafruit_GFX.h"
-#include "neopixel/neopixel.h"
+#include "../neopixel/neopixel.h"
 
 // Matrix layout information is passed in the 'matrixType' parameter for
 // each constructor (the parameter immediately following is the LED type
